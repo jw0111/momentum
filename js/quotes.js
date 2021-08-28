76139767cf21a084f361843bf90fe4b28631697b
jw0111 <59572441+jw0111@users.noreply.github.com>
2021-08-28 17:39:20 +0900

    Create Greetings And Clock

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+  {
+    quote: "To be happy, we must not be too concerned with others.",
+    author: "Albert Camus",
+  },
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    quote:
+      "Happiness is not something ready-made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    quote: "Happy people plan actions, they don't plan results.",
+    author: "Dennis Waitley",
+  },
+  {
+    quote: "The two enemies of human happiness are pain and boredom.",
+    author: "Arthur Schopenhauer",
+  },
+  {
+    quote: "Happiness is a state of activity",
+    author: "Aristotle",
+  },
+  {
+    quote: "To fall in love with yourself is the first secret to happiness.",
+    author: "Robert Morley",
+  },
+  {
+    quote: "The best proof of love is trust.",
+    author: "Joyce Brothers",
+  },
+  {
+    quote: "Not he who has mush is rich, but he who gives much.",
+    author: "Erich Fromm",
+  },
+  {
+    quote: "We acquire the strength we have overcome.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote: "Beauty begins the moment you decided to be yourself.",
+    author: "Coco Chanel",
+  },
+  {
+    quote: "Friends show their love in times of trouble, not in happiness.",
+    author: "Euripides",
+  },
+];
